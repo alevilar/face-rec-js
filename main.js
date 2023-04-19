58424b9292c184f3c42a6138db946845f3a99429
Ale Vilar <alevilar@gmail.com>
2023-04-18 21:56:43 -0300

    nueva version face api promises

--- main.js
+++ main.js
@@ -28,45 +28,80 @@ function image(img) {
     return result;
 }
 
+
+async function armarDescriptoresDelLabel(personPath, personPicture) {
+    const img = image(path.join(personPath, personPicture));
+    const faceTensor = await faceapi
+        .detectSingleFace(img)
+        .withFaceLandmarks()
+        .withFaceDescriptor();
+
+    if ( !faceTensor ) {
+        console.error("No se detectó ninguna cara en la foto: ", personPath)
+        return null;
+    } else {
+        const faceDescriptor = faceTensor.descriptor;
+        return faceDescriptor;
+    }
+}
+
+
+async function readFolder (tenantFolder, folderName) {
+    const personPath = path.join(tenantFolder, folderName);
+    
+    const faceDescriptors = await readFiles( personPath )
+
+    if ( faceDescriptors.length === 0 ) { 
+        // no hay cara encontrada
+        return null;
+    }
+
+    return new faceapi.LabeledFaceDescriptors(
+        folderName,
+        faceDescriptors
+    );
+}
+
+async function readFiles(personPath) {
+    const pics = fs.readdirSync(personPath)
+      .filter(
+        (fileName) =>
+            path.extname(fileName) === ".jpeg" ||
+            path.extname(fileName) === ".png" ||
+            path.extname(fileName) === ".jpg"
+        )
+
+    let descriptors = [];
+    
+    await Promise.all( pics.map( async (p) => {
+        const fd = await armarDescriptoresDelLabel(personPath, p);
+        if ( fd ) {
+            descriptors.push( fd )
+        }
+    }  ));
+
+    return descriptors;
+}
+
 async function trainModel(tenant) {
     // Lee las subcarpetas en la carpeta de "./train/{tenant}"
     let tenantFolder = path.join(TRAIN_DIR, tenant);
     const folders = fs.readdirSync(tenantFolder);
 
     // Crea un array de objetos con la ruta de cada foto y el nombre de la carpeta correspondiente
-    const labeledDescriptors = await Promise.all(
-        folders.map(async (folderName) => {
-            console.log(folderName);
-            const personPath = path.join(tenantFolder, folderName);
-            const faceDescriptors = await Promise.all(
-                fs
-                    .readdirSync(personPath)
-                    .filter(
-                        (fileName) =>
-                            path.extname(fileName) === ".jpeg" ||
-                            path.extname(fileName) === ".png" ||
-                            path.extname(fileName) === ".jpg"
-                    )
-                    .map(async (personPicture) => {
-                        const img = image(path.join(personPath, personPicture));
-                        const faceTensor = await faceapi
-                            .detectSingleFace(img)
-                            .withFaceLandmarks()
-                            .withFaceDescriptor();
-                        const faceDescriptor = faceTensor.descriptor;
-                        console.info(faceDescriptor);
-                        return faceDescriptor;
-                    })
-            );
-            console.info(faceDescriptors);
-            return new faceapi.LabeledFaceDescriptors(
-                folderName,
-                faceDescriptors
-            );
-        })
-    );
+    const labeledDescriptors = []
+
+    await Promise.all(folders.map( async (folderName) => {
+        const personLabeled = await readFolder(tenantFolder, folderName)
+        if ( personLabeled ) {
+            labeledDescriptors.push( personLabeled )
+        }
+    }))
+
+
+
     // Entrena el modelo con los datos de las fotos etiquetadas
-    console.info(labeledDescriptors);
+    console.info("labeled", labeledDescriptors);
     const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
 
     // Serializa el modelo entrenado en un archivo
